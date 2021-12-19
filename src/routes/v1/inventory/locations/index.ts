83c64f5b68775ae18f0bcabe82bed84ce6668d04
Jacob Miller <jacobmiller22@vt.edu>
2021-12-18 20:41:55 -0500

    locations route

---
+++ src/routes/v1/inventory/locations/index.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { DataController } from '../../../../controllers/index';
+import { EHttpMethod } from '../../../../interfaces/http';
+import { IInventoryItem, IInventoryItemMap, IInventoryLocationMap, TItemType } from '../../../../interfaces/Inventory';
+
+module.exports = async (req: Request, res: Response) => {
+  switch (req.method as EHttpMethod) {
+    case EHttpMethod.GET:
+      /** Get all inventory items */
+      res.setHeader('Content-Type', 'application/json');
+      const all: IInventoryLocationMap = await DataController().getLocations();
+      if (all == null) {
+        return res.status(500).end();
+      }
+      return res.status(200).json(all);
+
+    case EHttpMethod.POST:
+    /** Create a new Item */
+
+    // const new_item: IInventoryItem = req.body;
+
+    // const inventory_id = await DataController().createInventoryItem(new_item);
+
+    // if (inventory_id == null) {
+    //   return res.status(400).end('Item already exists');
+    // }
+
+    // return res.status(201).end();
+    case EHttpMethod.PUT:
+      /** Update an inventory item */
+      return;
+    default:
+      console.log('default');
+      return res.status(404).json();
+  }
+
+  res.status(200).json();
+};
