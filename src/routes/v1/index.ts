535c1fb67beb40473fe9a6b40df47ebc8acde906
Jacob Miller <jacobmiller22@vt.edu>
2021-12-18 20:44:26 -0500

    location routes being routed properly

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -6,6 +6,7 @@ v1Router.route('/').get(require('./root/test'));
 
 v1Router.route('/inventory').all(require('./inventory'));
 v1Router.route('/inventory/types').all(require('./inventory/types'));
+v1Router.route('/inventory/locations').all(require('./inventory/locations'));
 // .get(require('./inventory/all'))
 // .post(require('./inventory/all'))
 // .put(require('./inventory/all'));
