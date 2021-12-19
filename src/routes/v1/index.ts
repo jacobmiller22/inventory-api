baf59fb697d28eaeac9f2ee0bbe2faf765344dec
Jacob Miller <jacobmiller22@vt.edu>
2021-12-18 19:56:42 -0500

    Types route

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -4,7 +4,8 @@ const v1Router = Router();
 
 v1Router.route('/').get(require('./root/test'));
 
-v1Router.route('/inventory/all').all(require('./inventory/all'));
+v1Router.route('/inventory').all(require('./inventory'));
+v1Router.route('/inventory/types').all(require('./inventory/types'));
 // .get(require('./inventory/all'))
 // .post(require('./inventory/all'))
 // .put(require('./inventory/all'));
