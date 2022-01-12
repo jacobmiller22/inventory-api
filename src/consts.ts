ceae62a6d53cac90edbdc0db6c2c49ce097fb09a
Jacob Miller <jacobmiller22@vt.edu>
2022-01-12 22:17:56 +0000

    moved db file out of src

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
-export const DB_PATH = './src/db';
+export const DB_PATH = './db';
 export const ALL_FILE = 'all.json';
 export const LOCATIONS_FILE = 'locations.json';
 export const TYPES_FILE = 'types.json';
 
-export const IMAGE_DB_INCOMING = './src/db/images/incoming';
-export const IMAGE_DB_OUTGOING = './src/db/images/outgoing';
+export const IMAGE_DB_INCOMING = `${DB_PATH}/images/incoming`;
+export const IMAGE_DB_OUTGOING = `${DB_PATH}/images/outgoing`;
